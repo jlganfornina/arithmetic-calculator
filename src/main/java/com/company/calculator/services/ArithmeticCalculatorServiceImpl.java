f67bb2e4d761a21f3c50ec1bdaa41119570330a8
José Luis Ganfornina García <joseluis.ganfornina@vitro.bio>
2020-10-22 19:40:45 +0200

    - Se hace uso de la factoría en el servicio

--- src/main/java/com/company/calculator/services/ArithmeticCalculatorServiceImpl.java
+++ src/main/java/com/company/calculator/services/ArithmeticCalculatorServiceImpl.java
@@ -1,9 +1,8 @@
 package com.company.calculator.services;
 
-import com.company.calculator.exceptions.InvalidOperationTypeException;
 import com.company.calculator.exceptions.InvalidTermException;
-import com.company.calculator.services.operations.ArithmeticCalculatorAdditionOperation;
-import com.company.calculator.services.operations.ArithmeticCalculatorSubtractOperation;
+import com.company.calculator.services.operations.ArithmeticCalculatorOperation;
+import com.company.calculator.services.operations.ArithmeticCalculatorOperationFactory;
 import com.company.calculator.services.operations.ArithmeticOperationType;
 import org.springframework.stereotype.Service;
 
@@ -12,36 +11,28 @@ import java.util.Objects;
 
 @Service
 public class ArithmeticCalculatorServiceImpl implements ArithmeticCalculatorService {
+    private final ArithmeticCalculatorOperationFactory arithmeticCalculatorOperationFactory;
+
+    public ArithmeticCalculatorServiceImpl(final ArithmeticCalculatorOperationFactory arithmeticCalculatorOperationFactory) {
+        this.arithmeticCalculatorOperationFactory = arithmeticCalculatorOperationFactory;
+    }
+
     @Override
     public BigDecimal executeOperation(final BigDecimal firstTerm,
                                        final BigDecimal secondTerm,
                                        final ArithmeticOperationType arithmeticOperationType) {
         validateTerm(firstTerm, InvalidTermException.FIRST_TERM_IS_NULL);
         validateTerm(secondTerm, InvalidTermException.SECOND_TERM_IS_NULL);
-        validateOperationType(arithmeticOperationType);
 
-        switch (arithmeticOperationType) {
-            case ADDITION:
-                final ArithmeticCalculatorAdditionOperation arithmeticCalculatorAdditionOperation = new ArithmeticCalculatorAdditionOperation();
-                return arithmeticCalculatorAdditionOperation.execute(firstTerm, secondTerm);
-            case SUBTRACT:
-                final ArithmeticCalculatorSubtractOperation arithmeticCalculatorSubtractOperation = new ArithmeticCalculatorSubtractOperation();
-                return arithmeticCalculatorSubtractOperation.execute(firstTerm, secondTerm);
-            default:
-                throw new InvalidOperationTypeException();
-        }
+        final ArithmeticCalculatorOperation arithmeticCalculatorAdditionOperation = arithmeticCalculatorOperationFactory.getStrategy(arithmeticOperationType);
+
+        return arithmeticCalculatorAdditionOperation.execute(firstTerm, secondTerm);
     }
 
     private void validateTerm(final BigDecimal term,
                               final String messageIfError) {
         if (Objects.isNull(term)) {
             throw new InvalidTermException(messageIfError);
-        }
-    }
-
-    private void validateOperationType(final ArithmeticOperationType arithmeticOperationType) {
-        if (Objects.isNull(arithmeticOperationType)) {
-            throw new InvalidOperationTypeException();
         }
     }
 }
--- src/test/java/com/company/calculator/services/ArithmeticCalculatorServiceImplTest.java
+++ src/test/java/com/company/calculator/services/ArithmeticCalculatorServiceImplTest.java
@@ -1,49 +1,65 @@
 package com.company.calculator.services;
 
-import com.company.calculator.exceptions.InvalidOperationTypeException;
 import com.company.calculator.exceptions.InvalidTermException;
+import com.company.calculator.services.operations.ArithmeticCalculatorOperation;
+import com.company.calculator.services.operations.ArithmeticCalculatorOperationFactory;
 import com.company.calculator.services.operations.ArithmeticOperationType;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class ArithmeticCalculatorServiceImplTest {
 
     @Test
     void shouldAddTwoNumbers() {
         // given
-        final ArithmeticCalculatorService arithmeticOperationsService = new ArithmeticCalculatorServiceImpl();
+        final ArithmeticOperationType arithmeticOperationType = ArithmeticOperationType.ADDITION;
+
+        final ArithmeticCalculatorOperation mockedArithmeticOperation = mock(ArithmeticCalculatorOperation.class);
+        final ArithmeticCalculatorOperationFactory mockedFactory = mock(ArithmeticCalculatorOperationFactory.class);
+        when(mockedFactory.getStrategy(arithmeticOperationType)).thenReturn(mockedArithmeticOperation);
+
+        final ArithmeticCalculatorService arithmeticOperationsService = new ArithmeticCalculatorServiceImpl(
+                mockedFactory);
 
         final BigDecimal firstTerm = new BigDecimal(String.valueOf(Math.random()));
         final BigDecimal secondTerm = new BigDecimal(String.valueOf(Math.random()));
-        final ArithmeticOperationType arithmeticOperationType = ArithmeticOperationType.ADDITION;
-        final BigDecimal expectedResult = firstTerm.add(secondTerm);
 
         // when
-        final BigDecimal result = arithmeticOperationsService.executeOperation(firstTerm, secondTerm, arithmeticOperationType);
+        arithmeticOperationsService.executeOperation(firstTerm, secondTerm, arithmeticOperationType);
 
         // then
-        assertThat(result).isEqualTo(expectedResult);
+        verify(mockedFactory, only()).getStrategy(arithmeticOperationType);
+        verify(mockedArithmeticOperation, only()).execute(firstTerm, secondTerm);
     }
 
     @Test
     void shouldSubtractTwoNumbers() {
         // given
-        final ArithmeticCalculatorService arithmeticOperationsService = new ArithmeticCalculatorServiceImpl();
+        final ArithmeticOperationType arithmeticOperationType = ArithmeticOperationType.SUBTRACT;
+
+        final ArithmeticCalculatorOperation mockedArithmeticOperation = mock(ArithmeticCalculatorOperation.class);
+        final ArithmeticCalculatorOperationFactory mockedFactory = mock(ArithmeticCalculatorOperationFactory.class);
+        when(mockedFactory.getStrategy(arithmeticOperationType)).thenReturn(mockedArithmeticOperation);
+
+        final ArithmeticCalculatorService arithmeticOperationsService = new ArithmeticCalculatorServiceImpl(
+                mockedFactory);
 
         final BigDecimal firstTerm = new BigDecimal(String.valueOf(Math.random()));
         final BigDecimal secondTerm = new BigDecimal(String.valueOf(Math.random()));
-        final ArithmeticOperationType arithmeticOperationType = ArithmeticOperationType.SUBTRACT;
-        final BigDecimal expectedResult = firstTerm.subtract(secondTerm);
 
         // when
-        final BigDecimal result = arithmeticOperationsService.executeOperation(firstTerm, secondTerm, arithmeticOperationType);
+        arithmeticOperationsService.executeOperation(firstTerm, secondTerm, arithmeticOperationType);
 
         // then
-        assertThat(result).isEqualTo(expectedResult);
+        verify(mockedFactory, only()).getStrategy(arithmeticOperationType);
+        verify(mockedArithmeticOperation, only()).execute(firstTerm, secondTerm);
     }
 
     @Test
@@ -52,14 +68,13 @@ class ArithmeticCalculatorServiceImplTest {
         final BigDecimal firstTerm = null;
         final BigDecimal secondTerm = new BigDecimal(String.valueOf(Math.random()));
         final ArithmeticOperationType arithmeticOperationType = ArithmeticOperationType.ADDITION;
+        final ArithmeticCalculatorOperationFactory mockedArithmeticCalculatorOperationFactory = mock(ArithmeticCalculatorOperationFactory.class);
 
-        final ArithmeticCalculatorService arithmeticCalculatorService = new ArithmeticCalculatorServiceImpl();
+        final ArithmeticCalculatorService arithmeticCalculatorService = new ArithmeticCalculatorServiceImpl(mockedArithmeticCalculatorOperationFactory);
 
         // when
         // then
-        assertThatCode(() -> {
-            arithmeticCalculatorService.executeOperation(firstTerm, secondTerm, arithmeticOperationType);
-        })
+        assertThatCode(() -> arithmeticCalculatorService.executeOperation(firstTerm, secondTerm, arithmeticOperationType))
                 .isExactlyInstanceOf(InvalidTermException.class)
                 .hasMessageContaining(InvalidTermException.FIRST_TERM_IS_NULL);
     }
@@ -70,29 +85,14 @@ class ArithmeticCalculatorServiceImplTest {
         final BigDecimal firstTerm = new BigDecimal(String.valueOf(Math.random()));
         final BigDecimal secondTerm = null;
         final ArithmeticOperationType arithmeticOperationType = ArithmeticOperationType.ADDITION;
+        final ArithmeticCalculatorOperationFactory mockedArithmeticCalculatorOperationFactory = mock(ArithmeticCalculatorOperationFactory.class);
 
-        final ArithmeticCalculatorService arithmeticCalculatorService = new ArithmeticCalculatorServiceImpl();
+        final ArithmeticCalculatorService arithmeticCalculatorService = new ArithmeticCalculatorServiceImpl(mockedArithmeticCalculatorOperationFactory);
 
         // when
         // then
         assertThatCode(() -> arithmeticCalculatorService.executeOperation(firstTerm, secondTerm, arithmeticOperationType))
                 .isExactlyInstanceOf(InvalidTermException.class)
                 .hasMessageContaining(InvalidTermException.SECOND_TERM_IS_NULL);
-    }
-
-    @Test
-    void shoudGetInvalidOperationTypeExceptionWhenSecondTermIsNull() {
-        // given
-        final BigDecimal firstTerm = new BigDecimal(String.valueOf(Math.random()));
-        final BigDecimal secondTerm = new BigDecimal(String.valueOf(Math.random()));
-        final ArithmeticOperationType arithmeticOperationType = null;
-
-        final ArithmeticCalculatorService arithmeticCalculatorService = new ArithmeticCalculatorServiceImpl();
-
-        // when
-        // then
-        assertThatCode(() -> arithmeticCalculatorService.executeOperation(firstTerm, secondTerm, arithmeticOperationType))
-                .isExactlyInstanceOf(InvalidOperationTypeException.class)
-                .hasMessageContaining(InvalidOperationTypeException.ARITHMETIC_OPERATION_NOT_SUPPORTED);
     }
 }
